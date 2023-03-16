bed7c1c661c82377e3fd9eba945f633aa0b5385c
ValentinYgout <63228068+ValentinYgout@users.noreply.github.com>
2023-03-16 09:33:58 +0100

    Update search.js
    for loop main search

--- js/script/search.js
+++ js/script/search.js
@@ -1,27 +1,36 @@
+
 function searchRecipes(searchTerm, currentList) {
+    let result = []
+    for (let i = 0; i < currentList.length; i++) {
+        const recipe = currentList[i];
+        if (recipe.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+            recipe.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+            result.push(currentList[i])
 
-
-    return currentList.filter(recipe => {
-        // console.log(recipe.ingredients, recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm)), searchTerm)
-        return recipe.name.toLowerCase().includes(searchTerm)
-            || recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm))
-            || recipe.description.toLowerCase().includes(searchTerm)
-    })
-
+        }
+        else {
+            for (let j = 0; j < recipe.ingredients.length; j++) {
+                const ingredient = recipe.ingredients[j];
+                if (ingredient.ingredient.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+                    result.push(recipe);
+                    break;
+                }
+            }
+        }
+    }
+    return result
 }
 
 function searchWithFilters(searchTerm, currentList) {
-    // console.log(searchTerm,currentList,"inside function")
 
     return currentList.filter(recipe => {
-        // console.log(recipe.ingredients, recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm)), searchTerm)
+      
         return recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm))
             || recipe.ustensils.map(ustensils => ustensils.toLowerCase()).includes(searchTerm)
             || recipe.appliance.toLowerCase().includes(searchTerm)
     })
 
 }
-
 
 
 function filterAfterDeletingTag(searchTerms, allRecipes) {
@@ -35,50 +44,3 @@ function filterAfterDeletingTag(searchTerms, allRecipes) {
     return currentList
 }
 
-
-// function searchWithFilters(searchTerm, currentList) {
-//     let results = [];
-//     for (let i = 0; i < currentList.length; i++) {
-//         let recipe = currentList[i];
-//         let hasIngredient = false;
-//         let hasUstensil = false;
-//         let hasAppliance = false;
-
-//         for (let j = 0; j < recipe.ingredients.length; j++) {
-//             let ingredient = recipe.ingredients[j].ingredient.toLowerCase();
-//             if (ingredient.indexOf(searchTerm.toLowerCase()) !== -1) {
-//                 hasIngredient = true;
-//                 break;
-//             }
-//         }
-
-//         if (recipe.ustensils) {
-//             for (let j = 0; j < recipe.ustensils.length; j++) {
-//                 let ustensil = recipe.ustensils[j].toLowerCase();
-//                 if (ustensil.indexOf(searchTerm.toLowerCase()) !== -1) {
-//                     hasUstensil = true;
-//                     break;
-//                 }
-//             }
-//         }
-
-//         if (recipe.appliance) {
-//             let appliance = recipe.appliance.toLowerCase();
-//             if (appliance.indexOf(searchTerm.toLowerCase()) !== -1) {
-//                 hasAppliance = true;
-//             }
-//         }
-
-//         if (hasIngredient || hasUstensil || hasAppliance) {
-//             results.push(recipe);
-//         }
-//     }
-//     return results;
-
-// }
-
-// function filterAfterDeletingTag(searchTerms, allRecipes) {
-//     return searchTerms.reduce((currentList, searchTerm) => {
-//       return searchWithFilters(searchTerm, currentList);
-//     }, allRecipes);
-//   }
