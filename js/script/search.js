c493e7a0b3c5b3cab69ffd4e4cc56a2948b3533f
ValentinYgout <63228068+ValentinYgout@users.noreply.github.com>
2023-03-16 09:41:08 +0100

    filter main search
    filter main search

--- js/script/search.js
+++ js/script/search.js
@@ -10,16 +10,15 @@ function searchRecipes(searchTerm, currentList) {
 
 }
 function searchWithFilters(searchTerm, currentList) {
-
     return currentList.filter(recipe => {
-      
-        return recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm))
-            || recipe.ustensils.map(ustensils => ustensils.toLowerCase()).includes(searchTerm)
-            || recipe.appliance.toLowerCase().includes(searchTerm)
+        // console.log(recipe.ingredients, recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm)), searchTerm)
+        return recipe.name.toLowerCase().includes(searchTerm)
+            || recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()).some((ingredient) => ingredient.includes(searchTerm))
+            || recipe.description.toLowerCase().includes(searchTerm)
     })
 
 }
-
+  
 
 function filterAfterDeletingTag(searchTerms, allRecipes) {
     let currentList = allRecipes
@@ -31,4 +30,51 @@ function filterAfterDeletingTag(searchTerms, allRecipes) {
     }
     return currentList
 }
+
+// function searchWithFilters(searchTerm, currentList) {
+//     let results = [];    
+//     for (let i = 0; i < currentList.length; i++) {
+//         let recipe = currentList[i];    
+//         let hasIngredient = false;
+//         let hasUstensil = false;
+//         let hasAppliance = false;
+
+//         for (let j = 0; j < recipe.ingredients.length; j++) {
+//             let ingredient = recipe.ingredients[j].ingredient.toLowerCase();    
+//             if (ingredient.indexOf(searchTerm.toLowerCase()) !== -1) {
+//                 hasIngredient = true;    
+//                 break;
+//             }
+//         }
+
+//         if (recipe.ustensils) {
+//             for (let j = 0; j < recipe.ustensils.length; j++) {
+//                 let ustensil = recipe.ustensils[j].toLowerCase();    
+//                 if (ustensil.indexOf(searchTerm.toLowerCase()) !== -1) {
+//                     hasUstensil = true;    
+//                     break;
+//                 }
+//             }
+//         }
+
+//         if (recipe.appliance) {
+//             let appliance = recipe.appliance.toLowerCase();    
+//             if (appliance.indexOf(searchTerm.toLowerCase()) !== -1) {
+//                 hasAppliance = true;    
+//             }
+//         }
+
+//         if (hasIngredient || hasUstensil || hasAppliance) {
+//             results.push(recipe);    
+//         }
+//     }
+//     return results;
+
+// }
+
+// function filterAfterDeletingTag(searchTerms, allRecipes) {
+//     return searchTerms.reduce((currentList, searchTerm) => {
+//       return searchWithFilters(searchTerm, currentList);    
+//     }, allRecipes);
+//   }
 
